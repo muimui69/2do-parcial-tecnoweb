7da1dff37ef284142761941e21db5ab384af1b6f
muimui69 <moises_c2015@outlook.es>
2025-02-10 10:22:05 -0400

    completo

---
+++ MSVenta.Venta/Messages/CommandHandlers/VentaCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿////using System.Threading;
+////using System.Threading.Tasks;
+////using Aforo255.Cross.Event.Src.Bus;
+////using MediatR;
+////using MSVenta.Venta.Messages.Commands;
+////using MSVenta.Venta.Messages.Events;
+
+////namespace MSVenta.Venta.Messages.CommandHandlers
+////{
+////    public class VentaCommandHandler : IRequestHandler<VentaCreateCommand, bool>
+////    {
+////        private readonly IEventBus _bus;
+
+//        public VentaCommandHandler(IEventBus bus)
+//        {
+//            _bus = bus;
+//        }
+
+//        public Task<bool> Handle(VentaCreateCommand request, CancellationToken cancellationToken)
+//        {
+//            _bus.Publish(new VentaCreatedEvent(
+//                request.VentaId,
+//                request.ProductoId,
+//                request.Cantidad
+//            ));
+//            return Task.FromResult(true);
+//        }
+//    }
+//}
---
+++ MSVenta.Venta/Messages/Events/VentaCreatedEvent.cs
@@ -0,0 +1,18 @@
+﻿using Aforo255.Cross.Event.Src.Events;
+
+namespace MSVenta.Venta.Messages.Events
+{
+    public class VentaCreatedEvent : Event
+    {
+        public int VentaId { get; set; }
+        public int ProductoId { get; set; }
+        public int Cantidad { get; set; }
+
+        public VentaCreatedEvent(int ventaId, int productoId, int cantidad)
+        {
+            VentaId = ventaId;
+            ProductoId = productoId;
+            Cantidad = cantidad;
+        }
+    }
+}
